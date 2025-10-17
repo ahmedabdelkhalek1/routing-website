f5faffc66ba6f394e56402e8a72ddd8527f57c9f
medoooo163 <medoooo163@gmail.com>
2025-10-17 18:10:15 +0400

    Initial proxy commit 3

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// index.js
 const ProxyChain = require('proxy-chain');
+
 const server = new ProxyChain.Server({
-    port: process.env.PORT || 10000,
-    // authentication: { username: 'user', password: 'pass' } // optional
+    port: process.env.PORT || 10000
 });
+
 server.listen(() => {
-    console.log('Proxy server is listening');
+    console.log('Proxy server is listening on port ' + server.port);
 });
